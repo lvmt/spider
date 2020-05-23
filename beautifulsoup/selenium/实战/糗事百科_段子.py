22ee49dbd3c7b1c24e3f65cd7e94d561779eb10e
lvmt <13554221497@163.com>
2020-05-23 11:18:19 +0800

    selenium爬取糗事百科

--- beautifulsoup/selenium/实战/糗事百科_段子.py
+++ beautifulsoup/selenium/实战/糗事百科_段子.py
@@ -4,6 +4,7 @@
 
 import time 
 from selenium import webdriver 
+from pymongo import MongoClient
 
 
 """
@@ -14,28 +15,38 @@ from selenium import webdriver
 
 """
 获取标签文本：text
-获取href属性值：href
+获取href属性值：get_attribute("href")
 """
+
+def get_db():
+    client = MongoClient(host="localhost", port=27017)
+    db = client.spider
+    collection = db.qiushibaike_selenium
+    return collection 
 
 
 def get_text():
     content_list = driver.find_elements_by_class_name("main-list") 
     # print(content_list)
-
+    collection = get_db()
     for item in content_list:
         tm = item.find_element_by_class_name("fr").text
         title = item.find_element_by_class_name("title").text
         link = item.find_element_by_class_name("title").find_element_by_tag_name("a").get_attribute("href")
         text = item.find_element_by_class_name("content").text
+        url = driver.current_url
         
         out_dict = {
             "发表时间": tm,
             "文章标题": title,
             "文章完整连接": link,
-            "文章内容": text
+            "文章内容": text,
+            "url": url
         }
         
-        print(out_dict)
+        print("\033[31m将该段子写入数据库中\033[0m")
+        collection.insert_one(out_dict)
+        # print(out_dict)
     
 
 def get_next():
@@ -52,13 +63,14 @@ def get_next():
     
 if __name__ == "__main__":
     driver =  webdriver.Firefox()
-    driver.get("http://qiushidabaike.com/text_309.html") 
+    driver.get("http://qiushidabaike.com/text_280.html") 
     get_text()
     time.sleep(2)
 
 
     while get_next():
         get_text()
+        time.sleep(5)
     
     
     
