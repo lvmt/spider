37e062fcd8bbef340fa5cc7c3624e4ec84dd2e7b
lvmt <13554221497@163.com>
2020-05-24 18:29:21 +0800

    163邮件发送

--- beautifulsoup/selenium/实战/邮件发送.py
+++ beautifulsoup/selenium/实战/邮件发送.py
@@ -6,42 +6,54 @@
 利用163邮箱发邮件
 """
 
-import re
 import time
 from  selenium import webdriver 
-from bs4 import BeautifulSoup
+from getpass import getpass
 
 
-driver = webdriver.Firefox()
-driver.get("https://mail.163.com/") 
-driver.implicitly_wait(30)
+def loging(user, passwd, reciver, subject, content):
+    driver = webdriver.Firefox()
+    driver.get("https://mail.163.com/") 
+    driver.implicitly_wait(30)
 
 
-# 进入登陆iframe框架中
-driver.switch_to.frame(3) # 这个不知道咋就对了， 无语了。。。 
-driver.find_element_by_name('email').clear()
-driver.find_element_by_name('email').send_keys("13554221497")
-driver.find_element_by_name("password").clear()
-driver.find_element_by_name("password").send_keys("lmt921108")
-driver.find_element_by_id("dologin").click()
-driver.switch_to.default_content()   #  退出刚刚进入的iframe框架
+    # 进入登陆iframe框架中
+    driver.switch_to.frame(3)   # 这个不知道咋就对了， 无语了。。。 
+    driver.find_element_by_name('email').clear()
+    driver.find_element_by_name('email').send_keys(user)
+    driver.find_element_by_name("password").clear()
+    driver.find_element_by_name("password").send_keys(passwd)
+    driver.find_element_by_id("dologin").click()
+    driver.switch_to.default_content()   #  退出刚刚进入的iframe框架
+
+    # 点击写信
+    time.sleep(10)
+    driver.find_elements_by_css_selector("div nav div ul li")[1].click()   # 写信
+    time.sleep(20)
+    driver.find_element_by_class_name("nui-editableAddr-ipt").send_keys(reciver)
+    driver.find_elements_by_css_selector("div section header div div div div input")[2].send_keys(subject)
 
 
-
-driver.find_elements_by_css_selector("div nav div ul li")[1].click()   # 写信
-time.sleep(20)
-driver.find_element_by_class_name("nui-editableAddr-ipt").send_keys("lvmengting4480@novogene.com")
-driver.find_elements_by_css_selector("div section header div div div div input")[2].send_keys("selenium 测试")
-
-
-# 写信在iframe中
-driver.switch_to.frame(driver.find_element_by_class_name("APP-editor-iframe"))
-driver.find_element_by_css_selector("body").send_keys("hhhhhhh selenium 测试")
-driver.switch_to.default_content() 
+    # 信的正文内容在iframe中
+    driver.switch_to.frame(driver.find_element_by_class_name("APP-editor-iframe"))
+    driver.find_element_by_css_selector("body").send_keys(content)
+    driver.switch_to.default_content() 
 
 
-# 邮件发送
-driver.find_element_by_css_selector("div > header > div > div > div > span").click()
+    # 邮件发送
+    driver.find_element_by_css_selector("div > header > div > div > div > span").click()
+    
+    # 关闭浏览器
+    time.sleep(4)
+    driver.quit()
 
-# # time.sleep(4)
-# # driver.quit()
+
+if __name__ == "__main__":
+    user = input("user: ")
+    passwd = getpass()
+    reciver = input("接收者: ")
+    subject = input("主题: ")
+    content = input("内容: ")
+    
+    loging(user, passwd, reciver, subject, content)
+    
