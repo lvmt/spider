a3c0a05f210011690f3d61f3df375fcbcb121a0f
lvmt <13554221497@163.com>
2020-05-28 11:20:32 +0800

    selenium爬虫框架

--- beautifulsoup/selenium/实战/前程无忧3.py
+++ beautifulsoup/selenium/实战/前程无忧3.py
@@ -8,11 +8,11 @@
 
 from selenium import webdriver
 import time 
-import openpyxl
+from openpyxl import Workbook
 
 
-
-class QCWU(object):
+  
+class QianChen(object):
     
     def __init__(self,user,passwd,job,city):
         self.user = user
@@ -24,6 +24,12 @@ class QCWU(object):
     def run(self):
         """爬虫主函数 
         """
+
+        # #新建了一个表格，尚未保存
+        wb = Workbook()     
+        ws = wb.create_sheet("工作信息", index=0)   #新建工作表，位置为第2个sheet
+        rows = []
+
         # login
         driver = webdriver.Firefox()
         driver.implicitly_wait(10)
@@ -57,20 +63,29 @@ class QCWU(object):
             input(f'{self.city} 不在热门城市列表中，请手动点击选择城市后，按回车继续...')
         else:
             target.click()
-        driver.find_element_by_id('work_position_click_bottom_save').click()
+            driver.find_element_by_id('work_position_click_bottom_save').click()
         time.sleep(2)
         driver.find_element_by_css_selector('#searchForm button').click()
         time.sleep(2)
     
-        self.one_page_info(driver)
+        rows1 = self.one_page_info(driver)
+        rows.extend(rows1)
         if self.last_page(driver):    
         # 获取每一页的信息
-            self.one_page_info(driver)
+            rows2 = self.one_page_info(driver)
+            rows.extend(rows2)
+        rows.sort(key=lambda item:item['公司名称'])  # 对写出的结果按照公司名称进行排序
+        
+        for row in rows:
+            row_values = [value for value in row.values()]
+            ws.append(row_values)
+        wb.save("前程无忧_{self.job}_{self.city}.xlsx".format(**locals()))
         
         
     def one_page_info(self, driver):
         """获取每一页的详细信息
         """
+        rows = []
         jobs = driver.find_elements_by_css_selector('#resultList div[class="el"]')
         for job in jobs:
             fields = job.find_elements_by_css_selector('span')
@@ -92,12 +107,15 @@ class QCWU(object):
             info = driver.find_elements_by_css_selector('.job_msg')
             if info and len(info) == 1:
                 job_info_dict['职位要求'] = info[-1].text
-           
+                
             driver.close()
             driver.switch_to.window(main_window)
 
-          
+            # 将每一页的信息存入列表中
+            rows.append(job_info_dict)
             print(job_info_dict)
+        
+        return rows
     
     
     def last_page(self, driver):
@@ -113,7 +131,7 @@ class QCWU(object):
 
 
         
-QCWU('13554221497', 'lmt921108', '生物信息 ', '武汉').run()
+QianChen('13554221497', 'lmt921108', '生物信息 ', '武汉').run()
                 
                 
         
